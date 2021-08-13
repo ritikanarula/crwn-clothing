cee0d36101a3a10a59a66cd0392ed6e3f1ed3589
Ritika Narula <ritikanarula@Ritikas-MacBook-Pro.local>
2021-08-13 10:55:45 -0700

    adding header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import Header from './components/header/header.component.jsx';
 
 function App() {
   return (
     <div>
+      <Header />
       <HomePage></HomePage>
     </div>
   );
